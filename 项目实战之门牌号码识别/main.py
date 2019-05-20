05ad0737444035897a063dc0481834d1627ee3ae
Jamie Lannister <18311448483@163.com>
2019-05-20 15:35:13 +0800

    使用get_Variable比较好一点

--- 项目实战之门牌号码识别/main.py
+++ 项目实战之门牌号码识别/main.py
@@ -8,40 +8,42 @@ from scipy.misc import imshow, imread, imresize
 # ######################################################
 
 def vgg_network(x, n_class):
+
+    net_total = {}
     '''设计网络权重、偏置以及所使用这些变量进行的卷积、池化和全连接过程'''
     weight = {
-        'wc1_1': tf.Variable(tf.cast(np.random.random([3, 3, 3, 16]   ), dtype=tf.float32), 'wc1_1'),
-        'wc2_1': tf.Variable(tf.cast(np.random.random([3, 3, 16, 32]  ), dtype=tf.float32), 'wc2_1'),
-        'wc3_1': tf.Variable(tf.cast(np.random.random([3, 3, 32, 64]  ), dtype=tf.float32), 'wc3_1'),
-        'wc3_2': tf.Variable(tf.cast(np.random.random([3, 3, 64, 64]  ), dtype=tf.float32), 'wc3_2'),
-        'wc4_1': tf.Variable(tf.cast(np.random.random([3, 3, 64, 128] ), dtype=tf.float32), 'wc4_1'),
-        'wc4_2': tf.Variable(tf.cast(np.random.random([3, 3, 128, 128]), dtype=tf.float32), 'wc4_2'),
-        'wc5_1': tf.Variable(tf.cast(np.random.random([3, 3, 128, 256]), dtype=tf.float32),  'wc5_1'),
-        'wc5_2': tf.Variable(tf.cast(np.random.random([3, 3, 256, 256]), dtype=tf.float32), 'wc5_2'),
-
-        # 全连接层
-        'wfc_1': tf.Variable(tf.cast(np.random.random([1 * 1 * 256, 256]), dtype=tf.float32), 'wfc_1'),
-        'wfc_2': tf.Variable(tf.cast(np.random.random([1 * 1 * 256, 256]), dtype=tf.float32), 'wfc_2'),
-        'wfc_3': tf.Variable(tf.cast(np.random.random([256, n_class]), dtype=tf.float32),     'wfc_3'),
+        'wc1_1': tf.get_variable('wc1_1', [3, 3, 3, 16]   , dtype=tf.float32),
+        'wc2_1': tf.get_variable('wc2_1', [3, 3, 16, 32]  , dtype=tf.float32),
+        'wc3_1': tf.get_variable('wc3_1', [3, 3, 32, 64]  , dtype=tf.float32),
+        'wc3_2': tf.get_variable('wc3_2', [3, 3, 64, 64]  , dtype=tf.float32),
+        'wc4_1': tf.get_variable('wc4_1', [3, 3, 64, 128] , dtype=tf.float32),
+        'wc4_2': tf.get_variable('wc4_2', [3, 3, 128, 128], dtype=tf.float32),
+        'wc5_1': tf.get_variable('wc5_1', [3, 3, 128, 256], dtype=tf.float32),
+        'wc5_2': tf.get_variable('wc5_2', [3, 3, 256, 256], dtype=tf.float32),
+        # 
+        # # 全连接层
+        'wfc_1': tf.get_variable('wfc_1', [1 * 1 * 256, 256], dtype=tf.float32),
+        'wfc_2': tf.get_variable('wfc_2', [1 * 1 * 256, 256], dtype=tf.float32),
+        'wfc_3': tf.get_variable('wfc_3', [256, n_class],     dtype=tf.float32),
     }
 
     biase = {
-        'bc1_1': tf.Variable(tf.cast(np.random.random([16]),dtype=tf.float32),'bc1_1'),
+        'bc1_1': tf.get_variable('bc1_1', [16], dtype=tf.float32),
 
-        'bc2_1': tf.Variable(tf.cast(np.random.random([32]),dtype=tf.float32),'bc2_1'),
+        'bc2_1': tf.get_variable('bc2_1', [32], dtype=tf.float32),
 
-        'bc3_1': tf.Variable(tf.cast(np.random.random([64]),dtype=tf.float32),'bc3_1'),
-        'bc3_2': tf.Variable(tf.cast(np.random.random([64]),dtype=tf.float32),'bc3_2'),
+        'bc3_1': tf.get_variable('bc3_1', [64], dtype=tf.float32),
+        'bc3_2': tf.get_variable('bc3_2', [64], dtype=tf.float32),
 
-        'bc4_1': tf.Variable(tf.cast(np.random.random([128]), dtype=tf.float32), 'bc4_1'),
-        'bc4_2': tf.Variable(tf.cast(np.random.random([128]), dtype=tf.float32), 'bc4_2'),
+        'bc4_1': tf.get_variable('bc4_1', [128], dtype=tf.float32),
+        'bc4_2': tf.get_variable('bc4_2', [128], dtype=tf.float32),
 
-        'bc5_1': tf.Variable(tf.cast(np.random.random([256]),dtype=tf.float32), 'bc5_1'),
-        'bc5_2': tf.Variable(tf.cast(np.random.random([256]),dtype=tf.float32), 'bc5_2'),
-        # 全连接层,)
-        'bfc_1': tf.Variable(tf.cast(np.random.random([256]),dtype=tf.float32),     'bfc_1'),
-        'bfc_2': tf.Variable(tf.cast(np.random.random([256]),dtype=tf.float32),     'bfc_2'),
-        'bfc_3': tf.Variable(tf.cast(np.random.random([n_class]),dtype=tf.float32),'bfc_3'),
+        'bc5_1': tf.get_variable('bc5_1', [256],dtype=tf.float32,),
+        'bc5_2': tf.get_variable('bc5_2', [256],dtype=tf.float32,),
+
+        'bfc_1': tf.get_variable('bfc_1', [256],dtype=tf.float32,),
+        'bfc_2': tf.get_variable('bfc_2', [256],dtype=tf.float32,),
+        'bfc_3': tf.get_variable('bfc_3', [n_class],dtype=tf.float32,),
     }
 
     #############
@@ -51,57 +53,58 @@ def vgg_network(x, n_class):
     # 卷积过程
 
     net = tf.nn.conv2d(input=x, filter=weight['wc1_1'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc1_1']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc1_1']))
     # 局部响应归一化
     # net = tf.nn.lrn(net)
     # 池化
     net = tf.nn.max_pool(value=net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    net_total['conv_1'] = net
 
     #############
     # conv_2
     #############
     # 卷积
     net = tf.nn.conv2d(input=net, filter=weight['wc2_1'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc2_1']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc2_1']))
     # 池化
     net = tf.nn.max_pool(value=net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
+    net_total['conv_2'] = net
     #############
     # conv_3
     #############
     # 卷积
     net = tf.nn.conv2d(input=net, filter=weight['wc3_1'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc3_1']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc3_1']))
 
     net = tf.nn.conv2d(input=net, filter=weight['wc3_2'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc3_2']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc3_2']))
     # 池化
     net = tf.nn.max_pool(value=net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
+    net_total['conv_3'] = net
     #############
     # conv_4
     #############
     # 卷积
     net = tf.nn.conv2d(input=net, filter=weight['wc4_1'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc4_1']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc4_1']))
 
     net = tf.nn.conv2d(input=net, filter=weight['wc4_2'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc4_2']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc4_2']))
     # 池化
     net = tf.nn.max_pool(value=net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
+    net_total['conv_4'] = net
     #############
     # conv_5
     #############
     # # 卷积
     net = tf.nn.conv2d(input=net, filter=weight['wc5_1'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc5_1']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc5_1']))
 
     net = tf.nn.conv2d(input=net, filter=weight['wc5_2'], strides=[1, 1, 1, 1], padding='SAME')
-    net = tf.nn.leaky_relu(tf.nn.bias_add(net, biase['bc5_2']))
+    net = tf.nn.relu(tf.nn.bias_add(net, biase['bc5_2']))
     # 池化
     net = tf.nn.max_pool(value=net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
+    net_total['conv_5'] = net
     # 构建全连接部分的网络结构
     # 拉升
     net = tf.reshape(net, shape=[-1, weight['wfc_1'].get_shape()[0]])
@@ -110,12 +113,12 @@ def vgg_network(x, n_class):
     # fc1
     ######################
     net = tf.nn.relu(tf.matmul(net, weight['wfc_1']) + biase['bfc_1'])
-
+    net_total['fc1'] = net
     ######################
     # fc2
     ######################
     net = tf.nn.relu(tf.matmul(net, weight['wfc_2']) + biase['bfc_2'])
-
+    net_total['fc2'] = net
     ######################
     # fc3(out)
     ######################
@@ -123,7 +126,9 @@ def vgg_network(x, n_class):
 
     # 注意：全连接的最后一层输出所使用的激活函数是likelyhood-softmax
     # print('最后一层输出的值为', net[0])
-    return tf.matmul(net, weight['wfc_3']) + biase['bfc_3']
+    output = tf.matmul(net, weight['wfc_3']) + biase['bfc_3']
+    net_total['fc3'] = output
+    return output, net_total
 
 # ######################################################
 # 加载数据并进行数据预处理
@@ -174,9 +179,9 @@ y = tf.placeholder(tf.float32, [None, n_class])
 # ######################################################
 # 损失函数及优化
 # ######################################################
-pred = vgg_network(x, n_class)
+pred, layer_output = vgg_network(x, n_class)
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=pred))
+loss = tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y)  # 损失函数
 opt = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 
 pred_1 = tf.nn.softmax(pred)
@@ -185,7 +190,7 @@ pred_1 = tf.nn.softmax(pred)
 #evaluation
 ######################
 acc_tf = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-acc = tf.reduce_mean(tf.cast(acc_tf, tf.float32), axis = None)
+acc = tf.reduce_mean(tf.cast(acc_tf, tf.float32))
 
 # ######################################################
 # 训练模型
@@ -196,9 +201,10 @@ config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
 
     epochs = 1000
-    batch_size = 256
+    batch_size = 1600 * 5
     display_epoch = 20
-
+    base_lr = 0.01
+    learning_rate = base_lr
     for epoch in range(epochs):
         sess.run(tf.global_variables_initializer())
 
@@ -206,24 +212,19 @@ with tf.Session(config=config) as sess:
         batch = data.shape[0] // batch_size
         for i in range(batch):
 
-            x_train = data[batch_size * i : batch_size * (i + 1)]
-            y_train = label[batch_size * i : batch_size * (i + 1)]
-            cost, accurate, _ = sess.run([loss, acc, opt], feed_dict={x:x_train, y:y_train, lr:1})
-            cost_.append(cost)
-            accurate_.append(accurate)
-            # print('预测值：', predict)
-            # cost_total.append(cost)
-        # if (epoch) % display_epoch == 0:
+            # x_train = data[batch_size * i : batch_size * (i + 1)]
+            # y_train = label[batch_size * i : batch_size * (i + 1)]
 
-        print('epoch:{}, cost:{}, acc:{}'.format(epoch, cost_[0], accurate_[0]))
+            X_train = data[i * batch_size:i * batch_size + batch_size]
+            Y_train = label[i * batch_size:i * batch_size + batch_size]
 
-            # plt.scatter(epoch, cost)
-            # plt.ion()
-            # plt.pause(0.2)
-            # plt.show()
-        # if epoch % 10 == 0:
-
-            # print('epochs:{}, cost:{}'.format(epoch, cost))
+            sess.run(opt, feed_dict={x: X_train, y: Y_train, lr: learning_rate})
+            cost, accuaray = sess.run([loss, acc], {x: X_train, y: Y_train, lr: learning_rate})
+            print('step:%s,loss:%f,acc:%f' % (str(epoch) + '-' + str(i), cost[0], accuaray))
+            # 动态修改学习率
+            learning_rate = base_lr * (1 - epoch / epochs) ** 2
+    save = tf.train.Saver()
+    save.save(sess, './resnet/门牌号码')
 
 
 
