3dfa14ccaad82a11e95e22872340ebd063870e86
Jamie Lannister <18311448483@163.com>
2019-05-30 19:55:51 +0800

    torch搭建纯前馈神经网络

--- torch/pytorch入门.py
+++ torch/pytorch入门.py
@@ -1,7 +1,43 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+x = torch.tensor([1, 2]).view(1, -1).float()
 
-# 创建一个0填充的矩阵
-x = torch.empty(5, 3)
+class Net(nn.Module):
 
-# print(x)
+    def __init__(self):
 
+        # super函数类的意思是将继承的父类里面的成员全部继承到本类中
+        super(Net, self).__init__()
+
+        self.w1 = nn.Linear(2, 10)
+        self.w2 = nn.Linear(10, 10)
+        self.w3 = nn.Linear(10, 2)
+
+    def forward(self, input):
+
+        net = F.relu(self.w1(x))
+        net = F.relu(self.w2(net))
+        out = self.w3(net)
+        return out
+# print(out)
+
+criterion = nn.MSELoss()
+y = torch.tensor([2, 1]).view(1, -1).float()
+
+net = Net()
+params = list(net.parameters())
+out = net(x)
+
+print('before update weight:', params[0])  # conv1's .weight
+# print('out:', out)
+# print('损失：', cost)
+
+# 优化
+optimizer = optim.SGD(net.parameters(), lr=10)
+optimizer.zero_grad()
+cost = criterion(out, y)
+cost.backward()
+optimizer.step()
+print('after update weight:', params[0])
